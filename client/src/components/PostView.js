78e2cd60736f3f873dfdd620d901b2bdcc4fb87b
Brianne Duffy <duffymbrianne@gmail.com>
2018-09-19 22:28:23 -0600

    not complete, but so tired

--- client/src/components/PostView.js
+++ client/src/components/PostView.js
@@ -1,7 +1,31 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import {
+  Divider,
+  Header,
+  Grid,
+  GridColumn,
+  Message, 
+  StatisticLabel,
+  FeedUser} from 'semantic-ui-react'
 
-const PostView = () => (
-  <div>Post View</div>
+const PostView = ({ post = {} }) => (
+
+  <Grid columns={3}>
+    <Grid.Column></Grid.Column>
+    <GridColumn textAlign="center">
+    <Link to="/posts">ALL POSTS</Link>
+      <div>
+        <p>{post.post}</p>
+      </div>
+    </GridColumn>
+    <GridColumn></GridColumn>
+  </Grid>
 )
 
-export default PostView
+const mapStateToProps = (state, props) => {
+  return { post: state.posts.find(p => p.id === parseInt(props.match.params.id)) }
+}
+
+export default connect(mapStateToProps)(PostView)
